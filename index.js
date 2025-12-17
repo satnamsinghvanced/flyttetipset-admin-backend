a2ba782ede28023b58b50febcbf75c1a482e18d3
Satnam <satnamsingh@vancedsolutions.com>
2025-12-17 13:16:56 +0530

    fix

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const cors = require('cors');
 const cron = require("./service/cronJob")
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
+const FRONTEND_URL = process.env.FRONTEND_URL || 'https://admin.flyttetipset.no/';
+app.use(cors({
+  origin: FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
+app.options('*', cors());
 
 
 app.use(express.json({ limit: "5mb" }));
